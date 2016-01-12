df4688819b5b6332ef518556963f139705b095d2
Oleg Ivashko <Oleg.Ivashko@yandex.ua>
2016-01-12 12:00:16 +0200

    Fix adding new entity

---
+++ src/main/java/io/github/oleiva/utils/DBInitializer.java
@@ -0,0 +1,51 @@
+//package io.github.oleiva.utils;
+//
+//import java.util.Date;
+//
+//import io.github.oleiva.dao.user.UserDao;
+//import io.github.oleiva.dao.newsentry.NewsEntryDao;
+//import io.github.oleiva.entity.NewsEntry;
+//import io.github.oleiva.entity.User;
+//
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//
+///**
+// * Initialize the database with some test entries.
+// */
+//
+//public class DBInitializer {
+//
+//	private NewsEntryDao newsEntryDao;
+//	private UserDao userDao;
+//	private PasswordEncoder passwordEncoder;
+//
+//	protected DBInitializer() {
+//		/* Default constructor for reflection instantiation */
+//	}
+//
+//	public DBInitializer(UserDao userDao, NewsEntryDao newsEntryDao, PasswordEncoder passwordEncoder) {
+//		this.userDao = userDao;
+//		this.newsEntryDao = newsEntryDao;
+//		this.passwordEncoder = passwordEncoder;
+//	}
+//
+//	public void initDataBase() {
+//		User userUser = new User("user", this.passwordEncoder.encode("user"));
+//		userUser.addRole("user");
+//		this.userDao.save(userUser);
+//
+//		User adminUser = new User("admin", this.passwordEncoder.encode("admin"));
+//		adminUser.addRole("user");
+//		adminUser.addRole("admin");
+//		this.userDao.save(adminUser);
+//
+//		long timestamp = System.currentTimeMillis() - 1000 * 60 * 60 * 24;
+//		for (int i = 0; i < 10; i++) {
+//			NewsEntry newsEntry = new NewsEntry();
+//			newsEntry.setContent("This is example content " + i);
+//			newsEntry.setDate(new Date(timestamp));
+//			this.newsEntryDao.save(newsEntry);
+//			timestamp += 1000 * 60 * 60;
+//		}
+//	}
+//}
